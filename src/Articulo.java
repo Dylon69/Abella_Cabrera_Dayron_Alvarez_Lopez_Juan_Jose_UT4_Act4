4bc5a7671dbc0b0d9121b0101dac363f70599f38
Juanjo <veltrox2002@gmail.com>
2026-02-11 23:25:10 +0000

    Modifcar la clase "Articulo" para gestionar los productos

---
+++ src/Articulo.java
@@ -0,0 +1,25 @@
+public class Articulo {
+
+    private String nombre;
+    private int cantidad;
+    private double precioUnidad;
+
+    public Articulo(String nombre, int cantidad, double precioUnidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precioUnidad = precioUnidad;
+    }
+
+
+    //Calcula el total del articulo concreto
+    public double getSubtotal() {
+        return cantidad * precioUnidad;
+    }
+
+    //Getters para que "Pedido" lea los datos
+    public String getNombre() { return nombre; }
+    public int getCantidad() { return cantidad; }
+    public double getPrecioUnidad() { return precioUnidad; }
+    }
+
+
